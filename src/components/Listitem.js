786740dc2d3a2203ea6ab3a9d0fc474b281a54fe
LSYbuilds <builddav2009@gmail.com>
2023-06-14 12:49:48 +0900

    feat : Todo title 수정 기능 추가

--- src/components/Listitem.js
+++ src/components/Listitem.js
@@ -1,15 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Listitem = ({ item, todoData, setTododata }) => {
-  console.log("Listitem 랜더링", item);
-  const btnStyle = {
-    color: "#000",
-    float: "right",
-    border: "none",
-    padding: "5px 9px",
-    borderRadius: "50%",
-    cursor: "pointer",
-  };
+  // console.log("Listitem 랜더링", item);
+  // 편집 상태 설정 state
+  const [isEdit, setIsEdit] = useState(false);
+  // 편집 상태 타이틀 설정 state
+  const [editTitle, setEditTitle] = useState(item.title);
   const getStyle = _completed => {
     return {
       padding: "10px",
@@ -18,13 +14,33 @@ const Listitem = ({ item, todoData, setTododata }) => {
     };
   };
 
-  const handleClick = _id => {
+  const handleDeleteClick = _id => {
     // 전달된 ID를 검색해서 목록에서 제거
     // 1. 전달된 id로 해당하는 목록 찾아서 제외
     // 2. 새로운 목록으로 갱신해서 화면 리랜더링
     // 3. 배열의 고차함수 중 filter를 사용
     const newTodoData = todoData.filter(item => item.id !== _id);
     setTododata(newTodoData);
+  };
+
+  const handleEditClick = () => {
+    setIsEdit(true);
+  };
+  const handleCancelClick = () => {
+    setIsEdit(false);
+  };
+  const handleSaveClick = _id => {
+    let newTodoData = todoData.map(item => {
+      if (item.id === _id) {
+        item.title = editTitle;
+      }
+      return item;
+    });
+    setTododata(newTodoData);
+    setIsEdit(false);
+  };
+  const handleEditChange = e => {
+    setEditTitle(e.target.value);
   };
 
   const handleCompleteChange = _id => {
@@ -37,24 +53,59 @@ const Listitem = ({ item, todoData, setTododata }) => {
     setTododata(newTodoData);
   };
 
-  return (
-    <div className="flex items-center justify-between w-full mb-2 px-4 -py-1 text-gray-600 bg-gray-100 botder rounded">
-      <div className="items-center" style={getStyle(item.completed)}>
-        {/* defaultChecked : 체크박스에 기본체크 상태 설정 */}
-        <input
-          type="checkbox"
-          defaultChecked={item.completed}
-          onChange={() => handleCompleteChange(item.id)}
-        />
-        <span className="item-title">{item.title}</span>
+  if (isEdit) {
+    // 편집중
+    return (
+      <div className="flex items-center justify-between w-full mb-2 px-4 -py-1 text-gray-600 bg-gray-100 botder rounded">
+        <div className="items-center w-3/5">
+          <input
+            className="w-full px-3 py-2 mr-3 text-grat-500  rounded"
+            type="text"
+            value={editTitle}
+            onChange={handleEditChange}
+          />
+          {/* <span className="item-title">{item.title}</span> */}
+        </div>
+        <div className="items-center">
+          <button className="px-4 py-2 float-right" onClick={handleCancelClick}>
+            Cancel
+          </button>
+          <button
+            className="px-4 py-2 float-right"
+            onClick={() => handleSaveClick(item.id)}
+          >
+            Save
+          </button>
+        </div>
       </div>
-      <div className="items-center">
-        <button style={btnStyle} onClick={() => handleClick(item.id)}>
-          X
-        </button>
+    );
+  } else {
+    // 일반상태
+    return (
+      <div className="flex items-center justify-between w-full mb-2 px-4 -py-1 text-gray-600 bg-gray-100 botder rounded">
+        <div className="items-center flex" style={getStyle(item.completed)}>
+          {/* defaultChecked : 체크박스에 기본체크 상태 설정 */}
+          <input
+            type="checkbox"
+            defaultChecked={item.completed}
+            onChange={() => handleCompleteChange(item.id)}
+          />
+          <span className="item-title ml-3">{item.title}</span>
+        </div>
+        <div className="items-center">
+          <button
+            className="px-4 py-2 float-right"
+            onClick={() => handleDeleteClick(item.id)}
+          >
+            Delete
+          </button>
+          <button className="px-4 py-2 float-right" onClick={handleEditClick}>
+            Edit
+          </button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 // 리랜더링 최적화 적용
 export default React.memo(Listitem);
